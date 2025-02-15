6246f3f980e0df4bb15691bb721b1dfa3aba7c88
Fritz <orelawanson@gmail.com>
2025-02-15 23:59:30 +1100

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
-import Navbar from './components/Navbar'
 import RootLayout from './layout/RootLayout';
-import Home from './pages/Home'
-import Contact from './pages/Contact'
-import OurCharges from './pages/OurCharges'
-import Services from './pages/Services'
-
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import OurCharges from './pages/OurCharges';
+import Services from './pages/Services';
 
 const App = () => {
-// Router setup
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<RootLayout />}> 
-      <Route index element={<Home/>} />
-        <Route path='/OurCharges' element={<OurCharges />} />
-        <Route path='/Services' element={<Services />} />
-        <Route path='/Contact' element={<Contact />} />
+  // Router setup
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<Home />} />
+        <Route path="/OurCharges" element={<OurCharges />} />
+        <Route path="/Services" element={<Services />} />
+        <Route path="/Contact" element={<Contact />} />
       </Route>
-  )
-);
+    )
+  );
 
   return <RouterProvider router={router} />;
 };
 
-export default App
+export default App;
